3921ffeab65a12bac4d32ea64c3fba1e9f924b38
Heejin Cha <heejincha1219@gmail.com>
2015-01-22 13:29:45 +0900

    MultiChatServer 아마도.. 완성

--- MultiChatServer/src/com/cbnu/chatting/server/MultiChatServer.java
+++ MultiChatServer/src/com/cbnu/chatting/server/MultiChatServer.java
@@ -11,6 +11,8 @@ import java.util.Iterator;
 
 public class MultiChatServer {
 
+	private static final int SERVER_PORT = 3000;
+	
 	// 서버 소켓
 	private ServerSocket serverSock;
 	// 접속된 채팅 클라이언트 목록
@@ -18,31 +20,46 @@ public class MultiChatServer {
 		= new ArrayList<ChatThread>();
 	
 	// 채팅 서버 시작
-	public void start() {
+	public void start() throws IOException {
 		// 서버 소켓 생성
-		
+		serverSock = new ServerSocket(SERVER_PORT);
 		while (true) {
 			// 반복해서 클라이언트 연결 대기
+			Socket sock = serverSock.accept();
 			
 			// 연결 수립되면 채팅 스레드 생성하고
+			ChatThread ct = new ChatThread(sock);
+			
 			// 클라이언트 목록에 추가한 뒤
+			synchronized (clientList) {
+				clientList.add(ct);
+			}
+			
 			// 채팅 스레드 시작
+			ct.start();
 		}
 	}
 	
 	// 모든 클라이언트에게 메시지 전송
 	public void sendMsgAll(String msg) {
-		// 클라이언트를 순회하면서
-		Iterator<ChatThread> iter = clientList.iterator();
-		while (iter.hasNext()) {
-			// 각 클라이언트의 sendMsg(msg) 호출
-			iter.next().sendMsg(msg);	// iter.next()가 ChatThread 객체
+		synchronized (clientList) {
+			// 클라이언트를 순회하면서
+			Iterator<ChatThread> iter = clientList.iterator();
+			while (iter.hasNext()) {
+				// 각 클라이언트의 sendMsg(msg) 호출
+				iter.next().sendMsg(msg);	// iter.next()가 ChatThread 객체
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
 		MultiChatServer server = new MultiChatServer();
-		server.start();
+		try {
+			server.start();
+		} catch (IOException e) {
+			System.err.println("### 서버를 시작할 수 없습니다.");
+			e.printStackTrace();
+		}
 	}
 
 	/*** inner-class ChatThread ***/
@@ -55,6 +72,8 @@ public class MultiChatServer {
 		
 		private BufferedReader in;
 		private PrintWriter out;
+		
+		private String nick;
 		
 		public ChatThread(Socket sock) {
 			this.sock = sock;
@@ -83,31 +102,46 @@ public class MultiChatServer {
 					rmsg = msg.split("/");
 					// 2-1. "login" 메시지의 경우
 					if (rmsg[1].equals("login")) {
+						nick = rmsg[0];
 						// 모든 클라이언트에 "xxx님이 로그인했습니다."
-						
+						sendMsgAll("system/alert/" + nick + "님이 로그인했습니다.");
 					}
 					// 2-2. "logout" 메시지의 경우
 					else if (rmsg[1].equals("logout")) {
-						// 모든 클라이언트에 "xxx님이 로그아웃했습니다."
-						
+						/*
 						// 클라이언트 목록에서 이 스레드(this) 제거
-						
+						synchronized (clientList) {
+							clientList.remove(this);
+						}
+						// 모든 클라이언트에 "xxx님이 로그아웃했습니다."
+						sendMsgAll("system/alert/" + rmsg[0] + "님이 로그아웃했습니다.");
+						*/
+						break;
 					}
 					// 2-3. 일반 메시지의 경우
 					else if (rmsg[1].equals("chat")) {
 						// 모든 클라이언트에 rmsg[2]를 전송
-						
+						sendMsgAll(rmsg[2]);
 					}
 					// 2-4. 알 수 없는 메시지 유형
 					else {
 						// 화면에 수신 메시지 포함한 오류 메시지 출력
-						
+						System.err.println("알 수 없는 메시지("
+								+ sock.getInetAddress().getHostAddress() + "): "
+								+ msg);
 					}
 				}
 			} catch (IOException e) {
-				// 클라이언트 목록에서 이 스레드(this)를 제거
-				
 				e.printStackTrace();
+			} finally {
+				// 클라이언트 목록에서 이 스레드(this) 제거
+				synchronized (clientList) {
+					clientList.remove(this);
+				}
+				// 모든 클라이언트에 "xxx님이 로그아웃했습니다."
+				sendMsgAll("system/alert/" + nick + "님이 로그아웃했습니다.");
+				
+				status = false;
 			}
 		}
 		
